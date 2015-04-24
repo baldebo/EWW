bb8842e980e6919ddf276810a985c5120635825d
Max Enelund <spam@ig4.se>
2015-04-24 19:39:20 +0200

    Parson JSON Results in the works. Functionality is achieved.

--- app/src/main/java/nu/here/is/teampinez/weatherwarning/SearchActivity.java
+++ app/src/main/java/nu/here/is/teampinez/weatherwarning/SearchActivity.java
@@ -2,21 +2,44 @@ package nu.here.is.teampinez.weatherwarning;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 
 public class SearchActivity extends Activity {
     final static String authid = "5fe4551a599447929a301bc183b83a26";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        Parser p = new Parser();
-        p.execute();
+        Parser p = new Parser(SearchActivity.this);
+        try {
+            //TODO Try to make nicer!
+            JSONArray jsonArray =  new JSONObject(p.execute().get(1000, TimeUnit.MILLISECONDS)).getJSONObject("RESPONSE").getJSONArray("RESULT").getJSONObject(0).getJSONArray("WeatherStation");
 
+            Log.d("JSON", jsonArray.toString());
+
+            for(int i=0; i<jsonArray.length(); i++) {
+                JSONObject station = jsonArray.getJSONObject(i);
+
+                Log.d("JSON Station >", station.getString("Name"));
+            }
+
+        } catch (JSONException | TimeoutException | ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 
