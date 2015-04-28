3092aecd45d02266c7254e52e28d23e24af10cc4
Patrik Liao <patrikliao@gmail.com>
2015-04-28 15:13:32 +0200

    Clicking the settings button will now lead you to settings!

--- app/src/main/java/nu/here/is/teampinez/weatherwarning/GUIActivity.java
+++ app/src/main/java/nu/here/is/teampinez/weatherwarning/GUIActivity.java
@@ -4,14 +4,30 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 
 public class GUIActivity extends Activity {
-
+    private static ImageButton imgButton_search;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainscreenactivity);
+        OnClickButtonListener();
+    }
+
+    public void OnClickButtonListener () {
+        imgButton_search = (ImageButton)findViewById(R.id.imgBtnSettings);
+        imgButton_search.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent("nu.here.is.teampinez.weatherwarning.SettingsActivity");
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 
     public void openSearch(View view) {
