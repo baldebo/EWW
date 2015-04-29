4a3dd51211d23486d8ce7d461b63be24b76e2804
gusdebbaja <gusdebbja@student.gu.se>
2015-04-29 22:18:00 +0200

    updated widget buttons to textview (makes it easier to update the values)

--- app/src/main/java/nu/here/is/teampinez/weatherwarning/MyAppWidgetProvider.java
+++ app/src/main/java/nu/here/is/teampinez/weatherwarning/MyAppWidgetProvider.java
@@ -5,12 +5,21 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Button;
 import android.widget.RemoteViews;
 
- public class MyAppWidgetProvider extends AppWidgetProvider {
+
+public class MyAppWidgetProvider extends AppWidgetProvider {
 
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         final int N = appWidgetIds.length;
+        Button getWindDirection;
+        final Button getWindSpeed;
+        Button getWindGust;
+        final Button getRoadTemperature;
+        final Button getStationName;
+
+
 
         // Perform this loop procedure for each App Widget that belongs to this provider
         for (int appWidgetId : appWidgetIds) {
@@ -21,13 +30,24 @@ import android.widget.RemoteViews;
             // Get the layout for the App Widget and attach an on-click listener
             // to the button
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_main);
-            views.setOnClickPendingIntent(R.id.locationBtn, pendingIntent);
+            views.setOnClickPendingIntent(R.id.stationName, pendingIntent);
             views.setOnClickPendingIntent(R.id.windDirectionBtn, pendingIntent);
             views.setOnClickPendingIntent(R.id.windSpeedBtn, pendingIntent);
             views.setOnClickPendingIntent(R.id.windGustBtn, pendingIntent);
             views.setOnClickPendingIntent(R.id.roadTemperatureBtn, pendingIntent);
 
+        SearchActivity searchActivity = new SearchActivity();
 
+        for (int i = 0; i < N; i++){
+
+            RemoteViews v = new RemoteViews(context.getPackageName(), R.layout.widget_main);
+            v.setTextViewText(R.id.roadTemperatureBtn, (CharSequence) searchActivity.txtRdTemp);
+            //v.setTextViewText(R.id.windDirectionBtn, (CharSequence) searchActivity.t);
+            v.setTextViewText(R.id.windSpeedBtn, (CharSequence) searchActivity.txtWndSpd);
+            //v.setTextViewText(R.id.windGustBtn, (CharSequence) searchActivity.txtRdTemp);
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+
+        }
 
             // Tell the AppWidgetManager to perform an update on the current app widget
             appWidgetManager.updateAppWidget(appWidgetId, views);
