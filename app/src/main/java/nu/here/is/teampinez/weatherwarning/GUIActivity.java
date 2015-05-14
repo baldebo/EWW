5a7b3e6de104c70f8a77fc287ab7ee9cf8cdae68
Emil Baldebo <ebaldebo@gmail.com>
2015-05-14 21:34:23 +0200

    Notification works, AGA libraries added.

--- app/src/main/java/nu/here/is/teampinez/weatherwarning/GUIActivity.java
+++ app/src/main/java/nu/here/is/teampinez/weatherwarning/GUIActivity.java
@@ -121,6 +121,24 @@ public class GUIActivity extends Activity {
             e.printStackTrace();
         }
 
+        //Air Temp
+        String airTemperature;
+        if(airTemp[0] != null) {airTemperature = Double.toString(Double.parseDouble(airTemp[0])) + "C";}
+        else {airTemperature = "Not available!";}
+
+
+        String windSpeed;
+        if(windSpd[0] != null) {windSpeed = Double.toString(Double.parseDouble(windSpd[0])) + "M/S";}
+        else{windSpeed = "Not available!";}
+
+        String roadTemperature;
+        if(roadTemp[0] != null) {roadTemperature = Double.toString(Double.parseDouble(roadTemp[0])) + "C";}
+        else {roadTemperature = "Not available!";}
+
+        String stnName;
+        if(stationName[0] != null) {stnName = stationName[0];}
+        else {stnName = "Not Available!";}
+
         mBuilder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.drawable.notification_warning)
                 .setContentTitle(jsonOutput[0])
@@ -131,33 +149,44 @@ public class GUIActivity extends Activity {
         inboxStyle.setBigContentTitle(jsonOutput[0]);
         for (String jsonResult : jsonOutput) {
             inboxStyle.addLine(jsonResult);
-            jsonOutput[0] = ("Name: " + stationName[0]);
-            jsonOutput[1] = ("Air Temperature: " + airTemp[0] + "C");
-            jsonOutput[2] = ("Road Temperature: " + roadTemp[0] + "C");
-            jsonOutput[3] = ("Wind Speed: " + windSpd[0] + " m/s");
+            jsonOutput[0] = ("Name: " + stnName);
+            jsonOutput[1] = ("Air Temperature: " + airTemperature);
+            jsonOutput[2] = ("Road Temperature: " + roadTemperature);
+            jsonOutput[3] = ("Wind Speed: " + windSpeed);
         }
         mBuilder.setStyle(inboxStyle);
 
         //To quantify the metrics that get pulled as Strings we parse them into ints then check if they are extreme,
         // if so then the notification vibrates, and the LED is red and plays a sound to warn the user, also now the display lights up
-        double airTemperature = Double.parseDouble(airTemp[0]);
-        double windSpeed = Double.parseDouble(windSpd[0]);
-        double roadTemperature = Double.parseDouble(roadTemp[0]);
+
+        double rTemp;
+        if (roadTemp[0] != null) {rTemp = Double.parseDouble(roadTemp[0]);}
+        else {rTemp = 2;}
+
+        double aTemp;
+        if (airTemp[0] != null) {aTemp = Double.parseDouble(airTemp[0]);}
+        else {aTemp = 2;}
+
+
+        double wSpd;
+        if (windSpd[0] != null ) {wSpd  = Double.parseDouble(windSpd[0]);}
+        else {wSpd = 0;}
+
         Uri uri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+            if (rTemp <= 0 || aTemp >= 10 || wSpd >= 2) {
+                mBuilder.setColor(16711680).setSound(uri).setVibrate(new long[]{1000, 1000, 1000, 1000, 1000})
+                        .setLights(Color.RED, 3000, 3000).setOngoing(true);
+                PowerManager.WakeLock screenOn = ((PowerManager) getSystemService(POWER_SERVICE)).newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, "example");
+                screenOn.acquire();
+                screenOn.release();
 
-        if (roadTemperature <= 0 || windSpeed > 2 || airTemperature >= 10) {
-            mBuilder.setColor(16711680).setSound(uri).setVibrate(new long[]{1000, 1000, 1000, 1000, 1000})
-                    .setLights(Color.RED, 3000, 3000).setOngoing(true);
-            PowerManager.WakeLock screenOn = ((PowerManager)getSystemService(POWER_SERVICE)).newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, "example");
-            screenOn.acquire();
-            screenOn.release();
+            }
 
-        }
 
         //Set to Top Priority because it involves weather emergency warnings (2 is the highest priority)
         mBuilder.setPriority(PRIORITY_MAX);
         // Creates an explicit intent for an Activity in your app
-        Intent resultIntent = new Intent(this, NotificationMain.class);
+        Intent resultIntent = new Intent(this, GUIActivity.class);
 
         // The stack builder object will contain an artificial back stack for the
         // started Activity.
