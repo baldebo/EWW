a2c0e4d20397e2d2e8955ac7fb94e0e478b97880
Jamel <gusdebbja@student.gu.se>
2015-05-21 18:17:46 +0200

    nothingworks

--- app/src/main/java/nu/here/is/teampinez/weatherwarning/DriveActivity.java
+++ app/src/main/java/nu/here/is/teampinez/weatherwarning/DriveActivity.java
@@ -1,10 +1,16 @@
 package nu.here.is.teampinez.weatherwarning;
 
 import android.app.Activity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.support.v4.app.NotificationCompat;
 import android.swedspot.automotiveapi.AutomotiveSignal;
 import android.swedspot.automotiveapi.AutomotiveSignalId;
 import android.swedspot.scs.data.SCSFloat;
@@ -14,6 +20,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.drive.Drive;
 import com.swedspot.automotiveapi.AutomotiveFactory;
@@ -41,11 +48,11 @@ import java.util.concurrent.TimeoutException;
 
 public class DriveActivity extends Activity {
     final static String authid = "5fe4551a599447929a301bc183b83a26";
-    String stationName[];
-    String airTemp[];
-    String roadTemp[];
-    String windSpd[];
-    String stationCoords[];
+    //String stationName[];
+    //String airTemp[];
+    //String roadTemp[];
+    //String windSpd[];
+    //String stationCoords[];
 
     ArrayList<Station> stations = new ArrayList<>();
 
@@ -172,6 +179,59 @@ public class DriveActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void sendNotification(String title, Double airTemperature, Double roadTemperature, Double windSpeed){
+        final int mId = 12345;
+        final int PRIORITY_MAX = 2;
+        final NotificationCompat.Builder mBuilder;
+        NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+        String[] jsonOutput = new String[5];
+
+            NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+            mBuilder = new NotificationCompat.Builder(this)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentTitle(title)
+                    .setContentText("Nothing dangerous at this time")
+                    .setOngoing(true);
+
+            //extended notification view
+            jsonOutput[0] = "Station";
+            inboxStyle.setBigContentTitle("Drag down for more info");
+
+            for (String jsonResult : jsonOutput) {
+
+                inboxStyle.addLine(jsonResult);
+                jsonOutput[1] = ("Air Temperature: " + airTemperature);
+                jsonOutput[2] = ("Road Temperature: " + roadTemperature);
+                jsonOutput[3] = ("Wind Speed: " + windSpeed);
+            }
+
+            mBuilder.setStyle(inboxStyle);
+            // Creates an explicit intent for an Activity in your app
+            Intent resultIntent = new Intent(this, DriveActivity.class);
+
+            // The stack builder object will contain an artificial back stack for the
+            // started Activity.
+            // This ensures that navigating backward from the Activity leads out of
+            // your application to the Home screen.
+            TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+            // Adds the back stack for the Intent (but not the Intent itself)
+            stackBuilder.addParentStack(GUIActivity.class);
+            // Adds the Intent that starts the Activity to the top of the stack
+            stackBuilder.addNextIntent(resultIntent);
+            PendingIntent resultPendingIntent =
+                    stackBuilder.getPendingIntent(
+                            0,
+                            PendingIntent.FLAG_UPDATE_CURRENT
+
+                    );
+
+            mBuilder.setContentIntent(resultPendingIntent);
+            mNotificationManager.notify(mId, mBuilder.build());
+            mBuilder.setOngoing(true);
+
+        }
+
     public void getWeather(Integer param) {
         Parser p = new Parser(this);
 
@@ -187,6 +247,7 @@ public class DriveActivity extends Activity {
             JSONArray jsonArray = new JSONObject(p.execute(param, 10000).get(1000, TimeUnit.MILLISECONDS)).getJSONObject("RESPONSE").getJSONArray("RESULT").getJSONObject(0).getJSONArray("WeatherStation");
 
             //Parse data.
+
 
             for (int i = 0; i < jsonArray.length(); i++) {
                 Station s = new Station();
@@ -213,7 +274,6 @@ public class DriveActivity extends Activity {
                 } else {
                     s.windSpeed = parsedStation.getJSONObject("Measurement").getJSONObject("Wind").getString("Force");
                 }
-
 
                 // Catch faulty road temperatures
 
@@ -246,14 +306,29 @@ public class DriveActivity extends Activity {
             }
         } catch (JSONException | TimeoutException | ExecutionException | InterruptedException e) {
             e.printStackTrace();
+            //sendNotification("Something is wrong", 9999.9, 9999.9, 9999.9);
         }
+
         String printAll = "";
         for (int i = 0; i < stations.size(); i++){
             printAll += stations.get(i).name;
             printAll += "\n";
         }
+
         txtStationDistance0.setText(printAll);
 
+/*        for (int i = 0; i < stations.size(); i++){
+
+        double numAirTemp = Double.parseDouble(stations.get(i).airTemp);
+        double numRoadTemp = Double.parseDouble(stations.get(i).roadTemp);
+        double numWindSpeed = Double.parseDouble(stations.get(i).windSpeed);
+
+        sendNotification(stations.get(i).name, numAirTemp, numRoadTemp, numWindSpeed);
+
+        }*/
+
+
+        sendNotification(stations.get(0).name, 12.2, 12.2, 12.2);
     }
 
     public double getDistance(double statLat, double statLon) {
@@ -282,6 +357,7 @@ public class DriveActivity extends Activity {
         Log.d("Bearing - C", String.valueOf(gps.getLatitude()));
     }
 
+
     public void averageBearing() {
         new Timer().scheduleAtFixedRate(new TimerTask() {
             @Override
@@ -297,6 +373,8 @@ public class DriveActivity extends Activity {
             }
         }, 0, 5000);
     }
+
+
 
 
     private class getAverageBearingTask extends AsyncTask<Double, Void, Double> {
@@ -349,6 +427,7 @@ public class DriveActivity extends Activity {
         String windSpeed;
     }
 }
+
 
 
 
